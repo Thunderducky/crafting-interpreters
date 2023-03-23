7ab7ad68d7f2ef28df028777cb1cca187f686c4c
Thunderducky <scottzillaster@gmail.com>
2023-03-23 20:10:52 +0000

    page 76

--- JLOX/buildAndRun.sh
+++ JLOX/buildAndRun.sh
@@ -1,2 +1,3 @@
 javac com/craftinginterpreters/lox/*.java
-java com.craftinginterpreters.lox.Lox ../lox-files/main.lox
+# java com.craftinginterpreters.lox.Lox ../lox-files/main.lox
+java com.craftinginterpreters.lox.AstPrinter
